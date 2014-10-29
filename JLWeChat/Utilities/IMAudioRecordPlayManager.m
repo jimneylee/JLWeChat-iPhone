d0cc699d08750cffc189a20c1f1f33e6906b5a28
jimneylee <jimneylee@gmail.com>
2014-10-30 07:38:40 +0800

    clean code

--- JLWeChat/Utilities/IMAudioRecordPlayManager.m
+++ JLWeChat/Utilities/IMAudioRecordPlayManager.m
@@ -91,7 +91,7 @@
 - (void)startRecord
 {
     NSString *cacheDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-    NSString *fileName = [IMUtil generateVoiceTimeKey];
+    NSString *fileName = [IMUtil generateAudioTimeKey];
     NSString *filePath = [cacheDir stringByAppendingPathComponent:fileName];
     self.recordUrlKey = fileName;
     self.recordFileURL = [NSURL fileURLWithPath:filePath];
